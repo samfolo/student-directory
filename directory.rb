37d55bc2ec024844da4c779dc4498df5c98e7291
Sam Folorunsho <s.folorunsho@hotmail.co.uk>
2019-10-17 11:16:33 +0100

    split the procedures into three methods

--- directory.rb
+++ directory.rb
@@ -12,9 +12,21 @@ students = [
   "Joffrey Baratheon",
   "Norman Bates"
 ]
-#  and then print them
-puts "The students of Villains Academy"
-puts "-------------"
-students.each { |student| puts student }
-#  finally, we print the total number of students
-puts "Overall, we have #{students.count} great students"
+
+#  current methods
+def print_header
+  puts "The students of Villains Academy"
+  puts "-------------"
+end
+
+def print(names)
+  names.each { |name| puts name }
+end
+
+def print_footer(names)
+  puts "Overall, we have #{names.count} great students"
+end
+#  nothing happens until we call the methods
+print_header
+print(students)
+print_footer(students)
