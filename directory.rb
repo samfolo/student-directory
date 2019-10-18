6058918527df0b91165082aa763da5b0339d843d
Sam Folorunsho <s.folorunsho@hotmail.co.uk>
2019-10-18 14:18:11 +0100

    add country validation (no framework/library)

--- directory.rb
+++ directory.rb
@@ -2,7 +2,10 @@
 
 module Formatting
   def capitalize_each
-    self.split.map(&:capitalize).join(' ')
+    self.split.map{ |word|
+      ["l'", "c'"].include?(word[0..1].downcase) ? (word[0..2].upcase! + word[3..-1].downcase) :
+      ["(us)", "(usa)", "usa", "(uk)", "uk"].include?(word.downcase) ? word.upcase : word.capitalize
+    }.join(' ')
   end
 
   private 
@@ -14,8 +17,36 @@ module Formatting
   def short_bar
     puts "-" * 53
   end
+
+  def goodbye
+    puts "-------------".center(51)
+    puts "Goodbye".center(51)
+    puts "-------------".center(51)
+    puts " ".center(50)
+    puts " ".center(50)
+  end
 end
 
+module Validation
+  def countries
+    #  avoiding external libraries for this exercise
+    countries = ["Afghanistan", "Albania", "Algeria", "Andorra", "Angola", "Anguilla", "Antigua and Barbuda", "Argentina", "Armenia", "Aruba", "Australia", "Austria", "Azerbaijan", "Bahamas",
+    "Bahrain", "Bangladesh", "Barbados", "Belarus", "Belgium", "Belize", "Benin", "Bermuda", "Bhutan", "Bolivia", "Bosnia and Herzegovina", "Botswana", "Brazil", "British Virgin Islands",
+    "Brunei", "Bulgaria", "Burkina Faso", "Burundi", "Cambodia", "Cameroon", "Canada", "Cape Verde", "Cayman Islands", "Chad", "Chile", "China", "Colombia", "Congo", "Cook Islands", "Costa Rica",
+    "Cote D Ivoire", "Croatia", "Cruise Ship", "Cuba", "Cyprus", "Czech Republic", "Denmark", "Djibouti", "Dominica", "Dominican Republic", "Ecuador", "Egypt", "El Salvador", "Equatorial Guinea",
+    "England", "Estonia", "Ethiopia", "Falkland Islands", "Faroe Islands", "Fiji", "Finland", "France", "French Polynesia", "French West Indies", "Gabon", "Gambia", "Georgia", "Germany", "Ghana",
+    "Gibraltar", "Greece", "Greenland", "Grenada", "Guam", "Guatemala", "Guernsey", "Guinea", "Guinea Bissau", "Guyana", "Haiti", "Honduras", "Hong Kong", "Hungary", "Iceland", "India",
+    "Indonesia", "Iran", "Iraq", "Ireland", "Isle of Man", "Israel", "Italy", "Jamaica", "Japan", "Jersey", "Jordan", "Kazakhstan", "Kenya", "Kuwait", "Kyrgyz Republic", "Laos", "Latvia",
+    "Lebanon", "Lesotho", "Liberia", "Libya", "Liechtenstein", "Lithuania", "Luxembourg", "Macau", "Macedonia", "Madagascar", "Malawi", "Malaysia", "Maldives", "Mali", "Malta", "Mauritania",
+    "Mauritius", "Mexico", "Moldova", "Monaco", "Mongolia", "Montenegro", "Montserrat", "Morocco", "Mozambique", "Namibia", "Nepal", "Netherlands", "Netherlands Antilles", "New Caledonia",
+    "New Zealand", "Nicaragua", "Niger", "Nigeria", "Norway", "Oman", "Pakistan", "Palestine", "Panama", "Papua New Guinea", "Paraguay", "Peru", "Philippines", "Poland", "Portugal",
+    "Puerto Rico", "Qatar", "Reunion", "Romania", "Russia", "Rwanda", "Saint Pierre and Miquelon", "Samoa", "San Marino", "Satellite", "Saudi Arabia", "Scotland", "Senegal", "Serbia",
+    "Seychelles", "Sierra Leone", "Singapore", "Slovakia", "Slovenia", "South Africa", "South Korea", "Spain", "Sri Lanka", "St Kitts and Nevis", "St Lucia", "St Vincent", "St. Lucia",
+    "Sudan", "Suriname", "Swaziland", "Sweden", "Switzerland", "Syria", "Taiwan", "Tajikistan", "Tanzania", "Thailand", "Timor L'Este", "Togo", "Tonga", "Trinidad and Tobago", "Tunisia",
+    "Turkey", "Turkmenistan", "Turks and Caicos", "Uganda", "Ukraine", "United Arab Emirates", "United Kingdom", "United States", "United States Minor Outlying Islands", "Uruguay",
+    "Uzbekistan", "Venezuela", "Vietnam", "Virgin Islands (US)", "Wales", "Yemen", "Zambia", "Zimbabwe"].map { |country| country.downcase.to_sym }
+  end
+end
 #  current classes
 
 class String
@@ -86,6 +117,7 @@ end
 # each cohort instance
 class Cohort
   include Formatting
+  include Validation
   attr_accessor :academy, :month, :students
 
   def initialize(month)
@@ -102,7 +134,7 @@ class Cohort
 
     #  get the first name
     name = gets.chomp.capitalize_each
-    name = "" if name == "abort"
+    name = "" if name == "Abort"
 
     #  while the name is not empty, repeat this code
     while !name.empty?
@@ -112,9 +144,9 @@ class Cohort
       
       #  get rest of the data for the student
       puts "Please enter #{ new_student.name }'s age"
-      puts "(Applicants must be at least 5 and 130 years of age)"
+      puts "(Applicants must be at least 5 and at most 130 years of age)"
       new_student.age = gets.chomp.to_i
-      until (18..130).to_a.include?(new_student.age.to_i)
+      until (5..130).to_a.include?(new_student.age.to_i)
         puts "Invalid entry"
         puts "Please enter #{ new_student.name }'s age"
         puts "(Applicants must be at least 5 and 130 years of age)"
@@ -125,7 +157,7 @@ class Cohort
       until ["M", "F", "NB", "O"].include?(new_student.gender)
         puts "Invalid entry"
         puts "Please choose  #{ new_student.name }'s gender"
-        puts "(M) Male / (F) Female / (NB) Non-Binary / (O) Other"
+        puts "(M) Male / (F) Female / (NB) Non-Binary / (O) Other / Prefer not to say"
         new_student.gender = gets.chomp.upcase
       end
       puts "Please enter #{ new_student.name }'s height (in centimeters)"
@@ -141,11 +173,11 @@ class Cohort
         new_student.height = gets.chomp
       end
       puts "Please enter #{ new_student.name }'s country of birth"
-      new_student.country_of_birth = gets.chomp.capitalize
-      until new_student.country_of_birth
-        puts "Invalid entry"
+      new_student.country_of_birth = gets.chomp.capitalize_each
+      until self.countries.include?(new_student.country_of_birth.downcase.to_sym)
+        puts "Invalid entry #{gets.chomp.capitalize_each}"
         puts "Please enter #{ new_student.name }'s country of birth"
-        new_student.country_of_birth = gets.chomp.capitalize
+        new_student.country_of_birth = gets.chomp.capitalize_each
       end
       puts "Please enter #{ new_student.name }'s disability status ( true / false )"
       new_student.is_disabled = gets.chomp
@@ -169,11 +201,7 @@ class Cohort
     end
 
       #  format results upon completion
-      puts "-------------".center(51)
-      puts "Goodbye".center(51)
-      puts "-------------".center(51)
-      puts " ".center(50)
-      puts " ".center(50)
+      goodbye
       puts "There are now #{ @students.count } students in the #{@month} cohort:"
       puts " "
       self.student_profiles
